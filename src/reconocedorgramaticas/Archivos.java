51384fb6cd7018d005766e2c7ffa9b1f39c9511b
Go Green Celular <Go Green Celular@GoGreenCelular>
2017-10-06 15:26:48 -0500

    manejo de archivos

--- src/reconocedorgramaticas/Archivos.java
+++ src/reconocedorgramaticas/Archivos.java
@@ -21,7 +21,7 @@ public class Archivos {
       try {
          // Apertura del fichero y creacion de BufferedReader para poder
          // hacer una lectura comoda (disponer del metodo readLine()).
-         archivo = new File ("\\gramatica");
+         archivo = new File ("\\gramatic a");
          fr = new FileReader (archivo);
          br = new BufferedReader(fr);
 
